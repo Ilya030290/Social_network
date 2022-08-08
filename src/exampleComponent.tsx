1db96055130169a9d7d6c7c78dc530be1d4074f5
Ilya030290 <anoshko90@mail.ru>
2022-08-08 11:56:29 +0300

    add example component

---
+++ src/exampleComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExampleComponent = () => {
+    return (
+        <div>
+            Example Component
+        </div>
+    );
+};
+
+export default ExampleComponent;
